2f2e10a0f65137eb66170a216d9c0a881e93104f
Cameron Wade <cameronw7534@gmail.com>
2024-03-11 01:11:33 +0000

    Lab 2.1 complete, uncompiled

--- src/main.cpp
+++ src/main.cpp
@@ -46,18 +46,30 @@
   float eqTemperament = pow(2.0,1.0/12.0);
 
   const uint32_t stepSizes [] = { 
-    pow(2,32) * (fnoteA / ( pow(  eqTemperament , 9.0  ) ) ) / (fSamp),  //C
-    pow(2,32) * (fnoteA / ( pow(  eqTemperament , 8.0  ) ) ) / (fSamp),  //C#
-    pow(2,32) * (fnoteA / ( pow(  eqTemperament , 7.0  ) ) ) / (fSamp),  //D
-    pow(2,32) * (fnoteA / ( pow(  eqTemperament , 6.0  ) ) ) / (fSamp),  //D#
-    pow(2,32) * (fnoteA / ( pow(  eqTemperament , 5.0  ) ) ) / (fSamp),  //E
-    pow(2,32) * (fnoteA / ( pow(  eqTemperament , 4.0  ) ) ) / (fSamp),  //F
-    pow(2,32) * (fnoteA / ( pow(  eqTemperament , 3.0  ) ) ) / (fSamp),  //F#
-    pow(2,32) * (fnoteA / ( pow(  eqTemperament , 2.0  ) ) ) / (fSamp),  //G
-    pow(2,32) * (fnoteA / ( pow(  eqTemperament , 1.0  ) ) ) / (fSamp),  //G#
-    pow(2,32) * (fnoteA * ( pow(  eqTemperament , 0.0  ) ) ) / (fSamp),   //A
-    pow(2,32) * (fnoteA * ( pow(  eqTemperament , 1.0  ) ) ) / (fSamp),   //A#
-    pow(2,32) * (fnoteA * ( pow(  eqTemperament , 2.0  ) ) ) / (fSamp)   //B
+    // pow(2,32) * (fnoteA / ( pow(  eqTemperament , 9.0  ) ) ) / (fSamp),  //C
+    // pow(2,32) * (fnoteA / ( pow(  eqTemperament , 8.0  ) ) ) / (fSamp),  //C#
+    // pow(2,32) * (fnoteA / ( pow(  eqTemperament , 7.0  ) ) ) / (fSamp),  //D
+    // pow(2,32) * (fnoteA / ( pow(  eqTemperament , 6.0  ) ) ) / (fSamp),  //D#
+    // pow(2,32) * (fnoteA / ( pow(  eqTemperament , 5.0  ) ) ) / (fSamp),  //E
+    // pow(2,32) * (fnoteA / ( pow(  eqTemperament , 4.0  ) ) ) / (fSamp),  //F
+    // pow(2,32) * (fnoteA / ( pow(  eqTemperament , 3.0  ) ) ) / (fSamp),  //F#
+    // pow(2,32) * (fnoteA / ( pow(  eqTemperament , 2.0  ) ) ) / (fSamp),  //G
+    // pow(2,32) * (fnoteA / ( pow(  eqTemperament , 1.0  ) ) ) / (fSamp),  //G#
+    // pow(2,32) * (fnoteA * ( pow(  eqTemperament , 0.0  ) ) ) / (fSamp),   //A
+    // pow(2,32) * (fnoteA * ( pow(  eqTemperament , 1.0  ) ) ) / (fSamp),   //A#
+    // pow(2,32) * (fnoteA * ( pow(  eqTemperament , 2.0  ) ) ) / (fSamp)   //B
+    static_cast<uint32_t>(pow(2,32) * (fnoteA / ( pow(  eqTemperament , 9.0  ) ) ) / (fSamp)),  //C
+    static_cast<uint32_t>(pow(2,32) * (fnoteA / ( pow(  eqTemperament , 8.0  ) ) ) / (fSamp)),  //C#
+    static_cast<uint32_t>(pow(2,32) * (fnoteA / ( pow(  eqTemperament , 7.0  ) ) ) / (fSamp)),  //D
+    static_cast<uint32_t>(pow(2,32) * (fnoteA / ( pow(  eqTemperament , 6.0  ) ) ) / (fSamp)),  //D#
+    static_cast<uint32_t>(pow(2,32) * (fnoteA / ( pow(  eqTemperament , 5.0  ) ) ) / (fSamp)),  //E
+    static_cast<uint32_t>(pow(2,32) * (fnoteA / ( pow(  eqTemperament , 4.0  ) ) ) / (fSamp)),  //F
+    static_cast<uint32_t>(pow(2,32) * (fnoteA / ( pow(  eqTemperament , 3.0  ) ) ) / (fSamp)),  //F#
+    static_cast<uint32_t>(pow(2,32) * (fnoteA / ( pow(  eqTemperament , 2.0  ) ) ) / (fSamp)),  //G
+    static_cast<uint32_t>(pow(2,32) * (fnoteA / (pow(   eqTemperament, 1.0   ) ) ) / (fSamp)),  //G#
+    static_cast<uint32_t>(pow(2,32) * (fnoteA * ( pow(  eqTemperament , 0.0  ) ) ) / (fSamp)),  //A
+    static_cast<uint32_t>(pow(2,32) * (fnoteA * ( pow(  eqTemperament , 1.0  ) ) ) / (fSamp)),   //A#
+    static_cast<uint32_t>(pow(2,32) * (fnoteA * ( pow(  eqTemperament , 2.0  ) ) ) / (fSamp)),   //B
   };   
       //must be .0 to prevent integer division?
 
@@ -86,8 +98,10 @@ void sampleISR() {
   //phaseAcc += localCurrentStepSize;
   phaseAcc += currentStepSize;
   int32_t Vout = (phaseAcc >> 24) - 128;
+  Vout = Vout >> (8 - sysState.knob[3]);    //knob[3] used for volumeControl, should zero based on shifts
       //Right-shift (divide by ) the phase accumlator and subtract , to scale the range to -2^7 <= Vout <= 2^7 -1:
   analogWrite(OUTR_PIN, Vout + 128);
+
   // In future, you will need to multiply and add signals, for example to implement a volume control or polyphony. That will be easier when samples have an offset of zero because the 
   //offset will be unaffected by mathematical operations. 
   //Meanwhile, the phase accumulator itself cannot have a zero offset because that would require a signed integer and the overflow of signed integers results in undefined behaviour in C and C++.
@@ -97,7 +111,46 @@ struct {    //store system state that is used in more than one thread
   std::bitset<32> inputs;  // only contains the input bitset. The only other global variable is currentStepSize, 
           //but keep that apart from sysState because it is accessed by an ISR and the synchronisation method will be different
   SemaphoreHandle_t mutex;  //access to inputs is multiple cycles, therefore a mutex protects it from the multicycle threads accesses  
+  int knob[4];
 } sysState;
+
+class Knob {    //TODO implement this
+  private:
+      int rotateVal;
+      int limUpper;
+      int limLower;
+      SemaphoreHandle_t mutex;
+  public:
+      // Constructor
+      Knob(int initialValue, int upperLimit, int lowerLimit) {    //In setup i.e. knob3, "Knob knob3(0,8,0)"  //for initial volume = 0, highest volume = 8, lowest = 0
+          rotateVal = initialValue;
+          limUpper = upperLimit;
+          limLower = lowerLimit;
+          mutex = xSemaphoreCreateMutex();
+      }
+      // Function to update the knob value
+      void updateVal(int delta) { //delta to be +-1
+          xSemaphoreTake(mutex, portMAX_DELAY);  // Lock the mutex (when out of scope it unlocks)
+          //xSemaphoreGive(sysState.mutex);   //TODO setup knob.mutex
+          rotateVal += delta;
+
+          // Check and limit the knob value within the specified range
+          if (rotateVal > limUpper) {
+              rotateVal = limUpper;
+          } else if (rotateVal < limLower) {
+              rotateVal = limLower;
+          }
+          xSemaphoreGive(mutex);
+      }
+
+      // Function to read the current knob value
+      int readVal() const {
+          xSemaphoreTake(mutex, portMAX_DELAY);  // Lock the mutex
+          int val = rotateVal;    //locking mutex requires one extra variable
+          xSemaphoreGive(mutex);
+          return val;
+      }
+};
 
 std::bitset<4> readCols(){
     //Allows for row 0 to be read, C C# D D#
@@ -181,9 +234,13 @@ void scanKeysTask(void * pvParameters) {        //code for scanning the keyboard
     // Loop through the rows of the key matrix
     // Read the columns of the matrix and store the result in sysState.inputs
     // Look up the phase step size for the key that is pressed and update currentStepSize
-  const TickType_t xFrequency = 50/portTICK_PERIOD_MS;
+  const TickType_t xFrequency = 20/portTICK_PERIOD_MS;    //now 20ms
   TickType_t xLastWakeTime = xTaskGetTickCount();
-  std::bitset<2> lastKnob1 = 0;
+  std::bitset<2> knob3 = 0;
+  std::bitset<2> previousKnob3 = 0;
+  std::bitset<4> inputShort;
+  int knobCount[4]; //could use bitset<3> and .to_uint, but would need to type conversion both ways often, not worth it
+  std::bitset<2> previousTransition = 0; //11 is negative, 01 positve, 00 no transition
   while (1) {   //Infinite loop - i.e independent thread
       //std::bitset<32> inputs; //superseded by sysState.inputs
       // std::bitset<4> inputs = readCols();
@@ -193,26 +250,50 @@ void scanKeysTask(void * pvParameters) {        //code for scanning the keyboard
         setRow(loopCount);
         delayMicroseconds(3); //needed due to parasitic cap
 
-        std::bitset<4> inputShort = readCols();
-        xSemaphoreTake(sysState.mutex, portMAX_DELAY);   // TODO FIXME broken
+        inputShort = readCols();
+        xSemaphoreTake(sysState.mutex, portMAX_DELAY);
         for (int i = 0; i < 4; ++i) {
             sysState.inputs[( (4*loopCount+1)-1 ) + i] = ~ inputShort[i];  //bit inversion
         }
         xSemaphoreGive(sysState.mutex);
       } 
-      lastKnob1[0] = sysState.inputs[12];
-      lastKnob1[1] = sysState.inputs[13];
+      xSemaphoreTake(sysState.mutex, portMAX_DELAY); 
+      knob3[0] = sysState.inputs[12];
+      knob3[1] = sysState.inputs[13];
+      knobCount = sysState.knob;
+
+      if (( (~previousKnob3[1] & ~previousKnob3[0]) & (~knob3[1] &  knob3[0]) || 
+            ( previousKnob3[1] &  previousKnob3[0]) & ( knob3[1] & ~knob3[0]) ||   //00 to 01 or 11 to 10 for +1
+            ((~previousKnob3[1] & ~previousKnob3[0]) & ( knob3[1] &  knob3[0]) & (previousTransition&0x1)) ||     //00 to 11 impossible transition
+            ((~previousKnob3[1] &  previousKnob3[0]) & ( knob3[1] &  knob3[0]) & (previousTransition&0x1)) ||     //01 to 11 impossible transition
+            (( previousKnob3[1] & ~previousKnob3[0]) & (~knob3[1] &  knob3[0]) & (previousTransition&0x1)) )      //10 to 01 impossible tansition 
+            & ( sysState.knob[3] < 8 )  ){  //8 max value for knob
+            sysState.knob[3] ++;
+            previousTransition = 0x1;
+      }else if
+        ( ( (~previousKnob3[1] &  previousKnob3[0]) & (~knob3[1] & ~knob3[0]) || 
+            ( previousKnob3[1] & ~previousKnob3[0]) & ( knob3[1] &  knob3[0]) ||    //01 to 00 or 10 to 11 for -1
+            (( previousKnob3[1] &  previousKnob3[0]) & (~knob3[1] & ~knob3[0]) & (previousTransition&0x3) ))     //11 to 00 impossible transition
+            & ( sysState.knob[3] > 0 )   ){   
+            sysState.knob[3] --;
+            previousTransition = 0x3;
+      }else{  //otherwise ignore cases
+            previousTransition = 0x0;
+      }   
+      previousKnob3 = knob3;
+
+      xSemaphoreGive(sysState.mutex);
   }
 }
 
 
-void displayUpdateTask(void * pvParameters){
+void displayUpdateTask(void * pvParameters){            //NEEDS WORK
   const TickType_t xFrequency = 100/portTICK_PERIOD_MS;
   TickType_t xLastWakeTime = xTaskGetTickCount();
   while(1){
       // put your main code here, to run repeatedly:
     // static uint32_t next = millis();
-    static uint32_t count = 0;
+    // static uint32_t count = 0;
 
     // while (millis() < next);  //Wait for next interval
 
@@ -220,7 +301,7 @@ void displayUpdateTask(void * pvParameters){
 
     vTaskDelayUntil( &xLastWakeTime, xFrequency );   //New delay to supersede the millis()<next
 
-    //Update display
+    // Update display
     u8g2.clearBuffer();         // clear the internal memory
     u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
     u8g2.drawStr(2,10,"Hello World!");  // write something to the internal memory, cursor starts at 2,10
@@ -230,17 +311,16 @@ void displayUpdateTask(void * pvParameters){
     std::string note = keyPressed();
 
     u8g2.setCursor(2,20);               //x, y: Pixel position for the cursor when printing Cursor 2 down, 20 from left 
-    //u8g2.print(inputs.to_ulong(),HEX);  //Print the output data in HEX encoding from the position the cursor is set to
-    //u8g2.print(keyPressed(inputs).to_ulong(), HEX);  //Print the output data in HEX encoding from the position the cursor is set to
+    //u8g2.drawStr(2,20, note.c_str());  //Print the output data in HEX encoding from the position the cursor is set to
 
-    //u8g2.print(note.c_str());  //Print the output data in HEX encoding from the position the cursor is set to
+    u8g2.print(note.c_str());  //Print the output data in HEX encoding from the position the cursor is set to
 
-    // u8g2.setCursor(2, 20);
-    // u8g2.print("Step Sizes: ");
+    u8g2.setCursor(2, 20);
+    u8g2.print("Step Sizes: ");
     u8g2.print(sysState.inputs.to_ulong(),HEX); 
     u8g2.sendBuffer();          // transfer internal memory to the display
 
-    //Toggle LED
+    // Toggle LED
     digitalToggle(LED_BUILTIN);
   }
 }
@@ -298,7 +378,7 @@ void setup() {
   TaskHandle_t displayUpdateTaskHandle = NULL;
   xTaskCreate(
   displayUpdateTask,		/* Function that implements the task */
-  "displayUpdate",		/* Text name for the task */
+  "displayUpdateTask",		/* Text name for the task */
   256,      		/* Stack size in words, not bytes */
   NULL,			/* Parameter passed into the task */  //pvParameters input NULL for now
   1,			/* Task priority */
@@ -313,6 +393,30 @@ void setup() {
 
 
 void loop() { //Typically left empty
-  
+  // static uint32_t next = millis();
+  // static uint32_t count = 0;
+
+  // while (millis() < next);  //Wait for next interval
+
+  // next += interval;
+
+
+  // //Update display
+  // u8g2.clearBuffer();         // clear the internal memory
+  // u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+  // u8g2.drawStr(2,10,"Hello World!");  // write something to the internal memory, cursor starts at 2,10
+  // // u8g2.print(count++);      //Iteration count
+  //   // std::bitset<4> inputs = readCols();
+
+  // u8g2.setCursor(2,20);               //x, y: Pixel position for the cursor when printing Cursor 2 down, 20 from left 
+  // //u8g2.print(inputs.to_ulong(),HEX);  //Print the output data in HEX encoding from the position the cursor is set to
+  // u8g2.drawStr(2, 20, keyPressed().c_str());
+  // //u8g2.print(note);  //Print the output data in HEX encoding from the position the cursor is set to
+
+  // u8g2.sendBuffer();          // transfer internal memory to the display
+
+
+  // //Toggle LED
+  // digitalToggle(LED_BUILTIN);
 }
 
